93eb5f5bac68f40578f134503504d5916ffa387c
Abujawaahir10 <abuudi142@gmail.com>
2025-09-04 10:50:59 +1200

    Print Hello World to the user

---
+++ MyFirstProject/src/HelloWorld/HelloWorldExample.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class HelloWorldExample {
+	
+	public static void main (String[] args) {
+		
+		// Output Hello World to the user
+		
+		System.out.println("Hello World");
+	}
+
+}
